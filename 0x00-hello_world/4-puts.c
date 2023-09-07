4b9fc45520855e0d5c5153ef329a255f44c47769
Andrew Attia <andrew_attia@hotmail.com>
2023-09-07 15:17:50 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 #!/bin/bash
-puts (with proper grammar, but the outcome is a piece of art,\n)
+
+#include<stdio.h>       //header section
+
+int main()             //main section
+{
+    puts (with proper grammar, but the outcome is a piece of art,\n)
+    return 0;
+}
