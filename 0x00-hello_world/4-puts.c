1528a3d42ffca4dfd053567e30957b0d46b47118
Andrew Attia <andrew_attia@hotmail.com>
2023-09-07 15:53:40 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-#include<stdio.h>       //header section
-
-int main()             //main section
-{
-    puts (with proper grammar, but the outcome is a piece of art,\n)
-    return 0;
+#include<stdio.h>
+int main(){
+  puts ("\"Programming is like building a multilingual puzzle\n");
+  return 0;
 }
