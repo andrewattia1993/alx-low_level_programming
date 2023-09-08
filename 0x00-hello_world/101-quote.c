bc274a4b4d78d63aac4f2fc66da89d55da4ea18d
Andrew Attia <andrew_attia@hotmail.com>
2023-09-08 10:25:08 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	Char quo{} = “and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”;
+	char quo{} = “and that piece of art is useful\” - Dora Korpar, 2015-10-19\n”;
 	
-	Write (1,quo,59)
+	write (1,quo,59)
 	return (1)
 }
