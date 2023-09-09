f10e2e506ab283809accd34a8fa75d3b01abb28d
Andrew Attia <andrew_attia@hotmail.com>
2023-09-09 00:48:27 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 48; n < 57; n++)
 	{
 		putchar(n);
 		putchar(',');
 		putchar (' ');
 	}
+	putchar (57);
 	putchar('\n');
 	return (0);
 }
