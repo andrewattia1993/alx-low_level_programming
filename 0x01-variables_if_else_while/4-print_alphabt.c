baf77199b7251bfca8798cf0d97dd589e8b1fe41
Andrew Attia <andrew_attia@hotmail.com>
2023-09-08 17:09:32 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * descreption: alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar (ch);
+		ch++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
