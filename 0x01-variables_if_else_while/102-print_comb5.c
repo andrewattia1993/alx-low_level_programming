d180cdd1ab045509af91ea35410019e277474fd5
Andrew Attia <andrew_attia@hotmail.com>
2023-09-10 15:36:11 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,23 @@ int main(void)
 {
 	int digit1 = 0, digit2;
 
-	while (digit1 <= 9)
+	while (digit1 <= 99)
 	{
 		digit2 = 0;
-		while (digit2 <= 9)
+		while (digit2 <= 99)
 		{
-			putchar (digit1 + 48);
-			putchar (digit2 + 48);
-			if (digit1 + digit2 != 17)
+			if (digit2 != digit1)
 			{
-				putchar (',');
-				putchar (' ');
+				putchar ((digit1 / 10) + 48);
+				putchar ((digit2 % 10) + 48);
+				putchar (' ')
+				putchar ((digit2 / 10) + 48)
+				putchar ((digit2 % 10) + 48)
+				if (digit1 != 99  digit2 != 99)
+				{
+					putchar (',');
+					putchar (' ');
+				}
 			}
 			digit2++;
 		}
