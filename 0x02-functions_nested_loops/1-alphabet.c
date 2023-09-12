55b19cb93c0f1a0b2f214195743ce1fc96c9b987
Andrew Attia <andrew_attia@hotmail.com>
2023-09-12 15:32:46 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * from 0 to 10
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar ('\n');
+	return (0);
+}
