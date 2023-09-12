16077bfc250ceb6bc4044bcf7bf3b62ec1ec3084
Andrew Attia <andrew_attia@hotmail.com>
2023-09-12 16:52:46 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs according to number value
+ * Return: 1 if n is greater than zero,
+ * 0 f n is zero , -1 if n is less than zero
+ * @n: the integer to check 
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		printf('+');
+		return (1);
+	else if (n == 0)
+		printf('0');
+		return (o);
+	else if (n < 0)
+		printf('-');
+		return (-1);
+	return (0);
+}
