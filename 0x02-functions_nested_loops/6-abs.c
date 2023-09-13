8c6e3935026ee9f33ce48949b9d814e140208f80
Andrew Attia <andrew_attia@hotmail.com>
2023-09-12 17:56:53 -0700

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs: function that gets the absoulte for an intger
+ * _abs -  function that gets the absoulte for an intger
  * @n: n is a intger taht is an input for function
  * Return: Always 0
  */
