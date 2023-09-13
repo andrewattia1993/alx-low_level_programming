f3de620fc00497f6e051c4d296782616b6629838
Andrew Attia <andrew_attia@hotmail.com>
2023-09-12 19:00:41 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ */
+void times_table(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((n * m) + '0');
+			_putchar((',') + '0');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
