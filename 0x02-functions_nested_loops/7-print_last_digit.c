2ecb22a715d6027a3648e4054052b243f6a7c522
Andrew Attia <andrew_attia@hotmail.com>
2023-09-12 18:13:12 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar('n');
-	return (0);
+	int last
+	if (n < o)
+		last = (-1) * (n % 10)
+	else
+		last = n % 10;
+
+	_putchar(last +'0');
+	return (last);
 }
