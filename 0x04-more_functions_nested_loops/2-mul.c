2ad9c29ee5e4237515f20121828c00a5d34ca4b5
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 14:17:30 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code.
+ * @a : an int
+ * @b : another int
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
