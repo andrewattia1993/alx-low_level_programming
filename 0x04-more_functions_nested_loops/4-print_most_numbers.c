d43e3ed98f35a6ab7933451a176cd7de9aaf4d79
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 14:29:16 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c : a character either upper or lower case
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	for (i = 0, i <= 9, i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+	return (0);
+}
