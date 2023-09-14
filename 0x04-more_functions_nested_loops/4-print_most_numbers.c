3104fda254c1158a426fba33aa54169845f1e1c3
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 16:16:22 -0700

    5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,6 @@
+#include <unistd.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - check the code.
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (c = o; c <= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
 			j = c;
 			if (c > 9)
