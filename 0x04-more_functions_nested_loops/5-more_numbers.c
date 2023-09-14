904d7200376dbd6efa4d15e0aa5de24b94cef8e8
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 16:56:00 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int i, j, c;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			j = c;
-			if (c > 10)
+			if (c > 9)
 			{
 				_putchar(1 + '0');
 				j = c % 10;
