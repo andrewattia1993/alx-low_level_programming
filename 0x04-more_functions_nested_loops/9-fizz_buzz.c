43eb46e70c4bc96f6bde41cebe099c91c0320b0d
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 19:21:52 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the range 0-14 ten times.
+ * Return: nothing.
+ */
+
+ int main(void)
+ {
+ 	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+		printf ("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf ("Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 ==0)
+	{
+		printf ("FizzBuzz");
+	}
+	else 
+	{
+		printf ("%d ", i);
+	}
+}
