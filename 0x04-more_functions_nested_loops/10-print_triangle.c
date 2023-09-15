fe2db310063ef191b52e2c5e187d8edac462e52c
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 18:58:17 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,11 +14,17 @@ void print_triangle(int size)
 	{
 		for (j = 1; j < size; j++)
 		{
-			_putchar(' ');
+			if (i + j < size)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+				_putchar('\n');
+			}
 		}
-		_putchar('#');
-		_putchar('\n');
 	}
 	if (size <= 0)
-	_putchar('\n');
+		_putchar('\n');
 }
