e0392dc23728a340758ef4e88546d1dabbdd1f77
Andrew Attia <andrew_attia@hotmail.com>
2023-09-14 19:29:49 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,29 +5,28 @@
  * Return: nothing.
  */
 
- int main(void)
+int main(void)
 {
 	int i;
 
- 	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-	if (i % 3 == 0)
-	{
-		printf ("Fizz");
+		if (i % 3 == 0)
+		{
+			printf ("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf ("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 ==0)
+		{
+			printf ("FizzBuzz");
+		}
+		else 
+		{
+			printf ("%d ", i);
+		}
 	}
-	else if (i % 5 == 0)
-	{
-		printf ("Buzz");
-	}
-	else if (i % 3 == 0 && i % 5 ==0)
-	{
-		printf ("FizzBuzz");
-	}
-	else 
-	{
-		printf ("%d ", i);
-	}
-
 	return (0);
-	}
 }
