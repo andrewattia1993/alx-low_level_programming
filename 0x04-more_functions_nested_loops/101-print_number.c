ac26fd2ad0bffc894db19b6a5378b3cd2abf4eed
Andrew Attia <andrew_attia@hotmail.com>
2023-09-15 18:17:25 -0700

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - check the code.
+ * @n: intger
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (num < 0)
+	{
+		_putchar("-");
+		num = -num;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
