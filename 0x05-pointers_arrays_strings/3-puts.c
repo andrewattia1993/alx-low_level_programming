09662ebaa24560130c07ff53873c405d8c178049
Andrew Attia <andrew_attia@hotmail.com>
2023-09-18 19:51:24 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - check the code.
+ * @str : string
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str = 0);
+		str++;
+	}
+	_putchar('\n');
+}
