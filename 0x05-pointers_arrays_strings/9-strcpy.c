18813cd8aa60f193624e76257f5e46ccacca9539
Andrew Attia <andrew_attia@hotmail.com>
2023-09-19 17:04:13 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - check the code.
+ * @dest : string
+ * @src : intger inside the array
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l, i;
+
+	l = 0;
+
+	while (l != '\0')
+	{
+		l++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+
+}
