6f64cb78d01bf77d395b17867269f44977d411cf
Andrew Attia <andrew_attia@hotmail.com>
2023-09-21 17:17:17 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest  : array
+ * @src : array
+ * @n : int
+ * Return: a pointer to the resulting string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+
+	while (dest[a])
+		a++;
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+		dest[b] = src[b];
+
+	return (dest);
+}
