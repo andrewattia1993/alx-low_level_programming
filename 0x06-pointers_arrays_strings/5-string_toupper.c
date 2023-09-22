fcfbdaa57771af1d1b4f0e32dc19c2e0fd1451e7
Andrew Attia <andrew_attia@hotmail.com>
2023-09-21 18:19:11 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,14 +2,14 @@
 
 /**
  * string_toupper - concatenates two strings
- * @*  : array
+ * @*str  : array
  * Return: a pointer to the resulting string
  **/
 
 char *string_toupper(char *str)
 {
 	int a;
-	
+
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (str[a] >= 97 && str[a] <= 122)
