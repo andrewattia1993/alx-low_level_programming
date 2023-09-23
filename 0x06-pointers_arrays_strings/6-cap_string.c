d2e6ad908a0aae2b9127975f24b3dc7bd2aaa8d8
Andrew Attia <andrew_attia@hotmail.com>
2023-09-22 17:39:42 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,13 +13,13 @@ char *cap_string(char *s)
 	char str[] = {'\t', '\n', ' ', ',', ';', '.', '!',
 		'?', '"', '(', ')', '{', '}'};
 
-	for (a = 0, s[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
 		{
 			s[a] -= 32;
 		}
-		for (b = 0; str[b] < 13, b++)
+		for (b = 0; str[b] < 13; b++)
 		{
 			if (s[a] == str[b])
 			{
