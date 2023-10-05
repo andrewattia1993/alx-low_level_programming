5c849627d4b6dc48d37d3d6b3e910eac7d475c9d
Andrew Attia <andrew_attia@hotmail.com>
2023-10-04 19:32:42 -0700

    11

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,9 +4,8 @@
 
 
 /**
- * count_word - helper function to count the number of words in a string
+ * count - helper function to count the number of words in a string
  * @s: string to evaluate
- *
  * Return: number of words
  */
 int count(char *s)
