0db2d308a294847b34cb1a2288be21f92d1d8361
Andrew Attia <andrew_attia@hotmail.com>
2023-10-07 16:29:38 -0700

    0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv [i]);
+	}
+	return (0);
+}
