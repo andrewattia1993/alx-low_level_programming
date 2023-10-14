9c504ceeaa8a7fc87a0679dbce787a2f42c084ad
Andrew Attia <andrew_attia@hotmail.com>
2023-10-13 17:11:01 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
 /**
  * array_iterator - executes a function given as a parameter
  * on each element of an array
- * @array: array to iterate 
+ * @array: array to iterate
  * @size: size of the array
  * @action: pointer to function used
  */
