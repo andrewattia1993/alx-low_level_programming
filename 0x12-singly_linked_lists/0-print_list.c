4524838a726ed301ecb464205f3203c7d6268530
Andrew Attia <andrew_attia@hotmail.com>
2023-10-20 19:01:50 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,41 +3,27 @@
 #include <stdio.h>
 
 /**
- * _strlen - check the code.
- * @s: char to be enetered
- * Return: Always 0.
+ * print_list - Prints list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
  */
 
-int _strlen(char *s)
-{
-	int c;
-
-	for (c = 0; *s != '\0'; s++)
-		c++;
-
-	return (c);
-}
-
-/**
- * print_list - function that prints all the elements of a list_t.
- * @h: pointer to the list.
- *
- * if str is NULL print [0] (nil).
- * You are allowed to use printf.
- *
- * Return: the number of nodes.
- * @return size_t
- */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
-	while(h)
+	size_t n = 0;
+
+	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str: "(nill)"  );
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		n++;
 		h = h->next;
-		i++;
 	}
-	return (1);
+
+	return (n);
 }
-
-
