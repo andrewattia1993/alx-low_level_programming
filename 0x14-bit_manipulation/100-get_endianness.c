7df2b11fd5d0139607c9d01ef65a5702a32ce9d3
Andrew Attia <andrew_attia@hotmail.com>
2023-10-27 19:06:45 -0700

    100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+
+	return(*(char *)&n);
+}
